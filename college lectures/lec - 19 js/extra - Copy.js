ac27132175e13559c0eb428c2334547f2ea6c8f6
Manish Suresh <manishkau6239@gmail.com>
2024-09-26 14:22:31 +0530

    twenty second commit

---
+++ college lectures/lec - 19 js/extra - Copy.js
@@ -0,0 +1,41 @@
+// console.log("manish kaushik");
+// var a = 10 ;
+// console.log(a);
+// var a = 20
+// console.log(a);
+// var a = 30
+// console.log(a); 
+// var a = 30
+// console.log(a); 
+
+// let a = 10;
+// let b = -5;
+// let c = 2.5;
+// console.log(typeof(a))
+// console.log(typeof(b))
+// console.log(typeof(c))
+// let d= "Manish Kauhsik";
+// console.log(typeof(d));
+
+// let x = false;
+// console.log(typeof(x));
+
+// let y;
+// console.log(typeof(y));
+
+// y=20;
+// console.log(y)
+// console.log(typeof(y))
+
+// z = null;
+// console.log(typeof(z));
+
+let i = 5;
+let j = 2;
+console.log(i+j);
+
+let e = 5;
+e += 2;
+console.log(e);
+
+manish kaushik manihs kauhsii 
